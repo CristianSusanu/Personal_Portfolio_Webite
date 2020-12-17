385c57ee6584695bf6eb0ce6958017f559744c66
CristianSusanu <68741934+CristianSusanu@users.noreply.github.com>
2020-12-17 23:31:06 +0200

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,9 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^eylf053t0pd58l)pvqrch^#km$_52n#1qe%8r@*vreky^yl!m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
